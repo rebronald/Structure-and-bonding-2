e4a7f4b9d68c6fbcabe215407a199e6ab3955248
RR <53237734+rebronald@users.noreply.github.com>
2023-02-23 12:12:20 +1300

    new site - S&B

---
+++ StructureAndBonding_S4_Thermochemistry/story_content/user.js
@@ -0,0 +1,237 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5qcuopBTGhe":
+        Script1();
+        break;
+      case "6GiDHs5y67X":
+        Script2();
+        break;
+      case "6gNQRx4rLk5":
+        Script3();
+        break;
+      case "68PvGMh39tx":
+        Script4();
+        break;
+      case "5ZnpkJKop3a":
+        Script5();
+        break;
+      case "5w9vz0VU5vw":
+        Script6();
+        break;
+      case "5wyN94FKWY4":
+        Script7();
+        break;
+      case "5XtOtesAEDt":
+        Script8();
+        break;
+      case "5tZwyOhFGPd":
+        Script9();
+        break;
+      case "6dyTASHzrpb":
+        Script10();
+        break;
+      case "6OlcjXoDzIC":
+        Script11();
+        break;
+      case "64m2qxUgpsc":
+        Script12();
+        break;
+      case "6Nh2YC8oX67":
+        Script13();
+        break;
+      case "5oCbsSicBd4":
+        Script14();
+        break;
+      case "5r7Vh3wG3q9":
+        Script15();
+        break;
+      case "6mDe4Awl2Zs":
+        Script16();
+        break;
+      case "695Y2oyPnRc":
+        Script17();
+        break;
+      case "6QSo96Ca5Y8":
+        Script18();
+        break;
+      case "6Vq9NwO4gXd":
+        Script19();
+        break;
+      case "5V8pHL84olE":
+        Script20();
+        break;
+      case "6K6tbrYtO8L":
+        Script21();
+        break;
+  }
+}
+
+function Script1()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script2()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("SB14_answer1_q1");
+
+p.SetVar("SB14_answer1_q1", textentry.replace(/\s/g,''));
+}
+
+function Script3()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script4()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("SB14_answer2_q2");
+
+p.SetVar("SB14_answer2_q2", textentry.replace(/\s/g,''));
+}
+
+function Script5()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script6()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("SB14_answer3_q3");
+
+p.SetVar("SB14_answer3_q3", textentry.replace(/\s/g,''));
+}
+
+function Script7()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script8()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("SB14_answer4_q4");
+
+p.SetVar("SB14_answer4_q4", textentry.replace(/\s/g,''));
+}
+
+function Script9()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script10()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script11()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script12()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("SB13_Q5_textentry");
+p.SetVar("SB13_Q5_textentry", textentry.replace(/\s/g,''));
+}
+
+function Script13()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script14()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("SB13_Q8_textentry");
+p.SetVar("SB13_Q8_textentry", textentry.replace(/\s/g,''));
+}
+
+function Script15()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script16()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("SB13_Q10_textentry");
+p.SetVar("SB13_Q10_textentry", textentry.replace(/\s/g,''));
+}
+
+function Script17()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script18()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+
+}
+
+function Script19()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("ThermoQuiz_Q6_TextEntry_part2");
+p.SetVar("ThermoQuiz_Q6_TextEntry_part2", textentry.replace(/\s/g,''));
+}
+
+function Script20()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+var p = GetPlayer();
+var textentry = p.GetVar("ThermoQuiz_Q7_TextEntr_part2");
+p.SetVar("SB14_answer4_q4", textentry.replace(/\s/g,''));
+}
+
+function Script21()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("ThermoQuiz_Q7_TextEntry_part2");
+p.SetVar("ThermoQuiz_Q7_TextEntry_part2", textentry.replace(/\s/g,''));
+}
+
